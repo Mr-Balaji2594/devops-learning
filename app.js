99bf355bba66ddb1d236325fbb7cf2620d04f176
Mr-Balaji2594 <balabalaji1125@gmail.com>
2026-03-31 10:17:28 +0530

    App.js Modified: Message was changed

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+app.get("/", (req, res) => {
+  res.send("Welcome to Dev2Guru Enterprise!");
 });
 
 app.listen(PORT, () => {
